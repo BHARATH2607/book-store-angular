3eb76a9a5c1e31cca66584b54c4e27622aace459
BHARATH2607 <mbkumar2607@gmail.com>
2023-05-27 19:59:42 +0530

    1.0
    UI

---
+++ bookstoreApp/src/app/public/public-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public.component';
+import { AllBooksComponent } from './components/all-books/all-books.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'public',
+    component: PublicComponent,
+    children: [
+      { path: 'all-books', component: AllBooksComponent },
+      { path: 'book-details/:bookId/author/:authorId', component: BookDetailsComponent },
+      { path: 'home', component: HomeComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PublicRoutingModule { }
---
+++ bookstoreApp/src/app/public/public.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public',
+  templateUrl: './public.component.html',
+  styleUrls: ['./public.component.scss'],
+})
+export class PublicComponent implements OnInit {
+  bookId: number = 1;
+  authorId: number = 10;
+
+  ngOnInit(): void {}
+}
