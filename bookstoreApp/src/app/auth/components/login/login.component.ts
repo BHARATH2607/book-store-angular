a40bd44a509797c72eda37c63d5cdd4589ec7673
BHARATH2607 <mbkumar2607@gmail.com>
2023-06-04 19:28:29 +0530

    bsa-1
    ui changes

--- bookstoreApp/src/app/auth/components/login/login.component.ts
+++ bookstoreApp/src/app/auth/components/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  constructor(private router : Router){}
+  ngOnInit(): void {}
+
+  goToSignup(): void{
+    this.router.navigate(['/auth/signup']);
+  }
+
+  goToBookDetails(): void{
+    this.router.navigate(['/public/book-details',10,'author',300]);
+  }
+
+  goToBookDetailss(id:number,authorId:number): void{
+    this.router.navigate(['/public/book-details',id,'author',authorId], {
+      queryParams: {name : 'BharathKumar', email:'sample@gmail.com'}
+    });
+  }
 }
--- bookstoreApp/src/app/public/components/book-details/book-details.component.ts
+++ bookstoreApp/src/app/public/components/book-details/book-details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  public numberId : number = 0;
+  public authorId : number = 0;
+  public name : string = '';
+  public email : string = '';
+
+  constructor(private route:ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(param => {
+      this.numberId = param['bookId'];
+      this.authorId = param['authorId']
+    });
+
+    this.route.queryParams.subscribe(queryparam => {
+      this.name = queryparam['name'];
+      this.email = queryparam['email']
+    });
+  }
 
 }
